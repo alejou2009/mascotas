1d3cf4cde8e375598f537309d4014de80a32d950
alejou2009 <88717298+alejou2009@users.noreply.github.com>
2021-08-25 12:29:54 -0500

    Update Mascotas.java

--- mascotas/src/mascotas/Mascotas.java
+++ mascotas/src/mascotas/Mascotas.java
@@ -17,6 +17,7 @@ public class Mascotas {
     public static void main(String[] args) {
         // TODO code application logic here
         // hola hola
+        //Hola 2 jou
         
     }
     
