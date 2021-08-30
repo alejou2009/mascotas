e1fe3613c037bb96a0d13292db98512c4dd0fc2a
alejou2009 <88717298+alejou2009@users.noreply.github.com>
2021-08-29 19:35:25 -0500

    Código de encapsulación, aún faltan detalles como comentar.

---
+++ mascotas/src/udem/edu/co/mascotas/Conejo.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udem.edu.co.mascotas;
+
+/**
+ *
+ * @author User
+ */
+public class Conejo {
+    
+    private int extremidades;
+    private String color;
+    private String nombre;
+
+    public Conejo(int extremidades, String color, String nombre) {
+        this.extremidades = extremidades;
+        this.color = color;
+        this.nombre = nombre;
+    }
+
+    public int getExtremidades() {
+        return extremidades;
+    }
+
+    public void setExtremidades(int extremidades) {
+        this.extremidades = extremidades;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
